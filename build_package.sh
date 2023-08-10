f2e3d67c5963fd62d8b1e9ed20cf85781e270d5c
Florian Daloze <fda@odoo.com>
2023-08-10 11:53:04 +0200

    0.1.0-alpha.0

--- build_package.sh
+++ build_package.sh
@@ -1,1 +1,7 @@
-./node_modules/@vscode/vsce package -o output
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+echo $PACKAGE_VERSION
+./node_modules/@vscode/vsce/vsce package -o odoo_$PACKAGE_VERSION
