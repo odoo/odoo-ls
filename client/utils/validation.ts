34cd521e8318a5999c3e97959d24dd4d04842475
Lina (liew) <liew@odoo.com>
2023-09-21 16:06:48 +0200

    add state versioning and validate state to add any new variables needed in the state

--- client/utils/validation.ts
+++ client/utils/validation.ts
@@ -1,4 +1,17 @@
 export const STATE_VERSION = 100
-export const state_structure = {
-    
+
+export const stateInit = {
+    "Odoo.configurations": {},
+    "Odoo.nextConfigId": 0,
+    "Odoo.stateVersion": STATE_VERSION,
+}
+
+export function getConfigurationStructure(id: number = 0) {
+    return {
+        "id": id,
+        "name": `New Configuration ${id}`,
+        "odooPath": "",
+        "addons": [],
+        "pythonPath": "python3",
+    }
 }
