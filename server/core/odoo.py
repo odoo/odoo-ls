4bb42e3f96ac1b1d7bdab488917e7b70a8cb83e9
Lina (liew) <liew@odoo.com>
2023-10-05 11:21:15 +0200

    release locks before server reset

--- server/core/odoo.py
+++ server/core/odoo.py
@@ -167,6 +167,8 @@ class Odoo():
         self.stop_init = True
 
     def reset(self, ls):
+        self.thread_access_condition.release()
+        self.write_lock.release()
         with Odoo.instance.acquire_write(ls):
             Odoo.instance = None
 
