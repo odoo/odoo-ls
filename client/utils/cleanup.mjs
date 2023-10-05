667127544c1ec1886da74c7f18d9e4106fca4b43
Lina (liew) <liew@odoo.com>
2023-10-05 16:30:30 +0200

    add support for windows on uninstall

--- client/utils/cleanup.mjs
+++ client/utils/cleanup.mjs
@@ -1,7 +1,18 @@
 import { execSync } from "child_process";
+import * as os from 'os';
+
 try {
     // Keep as a failsafe for open servers
-    execSync("for KILLPID in `ps ax | grep 'clean-odoo-lsp' | awk ' { print $1;}'`; do kill -15 $KILLPID; done");
+    switch (os.type()) {
+        case 'Windows_NT':
+            execSync('taskkill /F /FI "IMAGENAME eq *clean-odoo-lsp*"')
+            break;
+        case 'Darwin':
+        case 'Linux':
+            execSync("for KILLPID in `ps ax | grep 'clean-odoo-lsp' | awk ' { print $1;}'`; do kill -15 $KILLPID; done");
+            break;
+
+    }
 }
 catch (err) {
     console.log(err)
