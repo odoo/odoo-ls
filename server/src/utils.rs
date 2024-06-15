ce2bf49e4c9228d719ac3b2405cadd2a7aef120b
fda-odoo <fda@odoo.com>
2024-06-15 18:17:32 +0200

    fix paths resolution on windows

--- server/src/utils.rs
+++ server/src/utils.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{ffi::OsStr, fs, path::{Path, PathBuf}};
 
 #[macro_export]
 macro_rules! S {
@@ -7,14 +7,42 @@ macro_rules! S {
     };
 }
 
+#[cfg(target_os = "windows")]
 pub fn is_file_cs(path: String) -> bool {
-    match fs::canonicalize(path) {
-        Ok(canonical_path) => {
-            return fs::metadata(canonical_path).unwrap().is_file()
+    let mut p = Path::new(&path);
+    if p.exists() {
+        while p.parent().is_some() {
+            let mut found = false;
+            if let Ok(entries) = fs::read_dir(p.parent().unwrap()) {
+                for entry in entries {
+                    if let Ok(entry) = entry {
+                        if entry.file_name() == p.components().last().unwrap().as_os_str() {
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if !found {
+                return false;
+            }
+            p = p.parent().unwrap();
         }
-        Err(_err) => {
-            return false;
+        return true;
+    }
+    false
+}
+
+#[cfg(any(target_os = "linux", target_os = "macos", target_os = "unix"))]
+pub fn is_file_cs(path: String) -> bool {
+    let p = Path::new(&path);
+    if p.exists() {
+        match p.file_name().and_then(OsStr::to_str) {
+            Some(name) => name == path,
+            None => false
         }
+    } else {
+        false
     }
 }
 
