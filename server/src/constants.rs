a2e3e5c52b1a688a38605ef54b8bc25a19f1d6f4
Florian Daloze <fda@odoo.com>
2024-11-01 02:06:58 +0100

    0.2.7

--- server/src/constants.rs
+++ server/src/constants.rs
@@ -2,7 +2,7 @@
 use core::fmt;
 
 pub const EXTENSION_NAME: &str = "Odoo";
-pub const EXTENSION_VERSION: &str = "0.2.6";
+pub const EXTENSION_VERSION: &str = "0.2.7";
 
 pub const DEBUG_ODOO_BUILDER: bool = false;
 pub const DEBUG_MEMORY: bool = false;
