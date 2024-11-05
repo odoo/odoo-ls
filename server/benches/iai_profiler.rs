49cb88b9635672da62e3a0f1440ba5325b6ae182
Florian Daloze <fda@odoo.com>
2024-11-05 16:13:45 +0100

    [IMP] server: run bench in single thread

--- server/benches/iai_profiler.rs
+++ server/benches/iai_profiler.rs
@@ -2,7 +2,7 @@ use lsp_server::Notification;
 use serde_json::json;
 use odoo_ls_server::{args::{Cli, LogLevel}, cli_backend::CliBackend, constants::*, server::Server, utils::PathSanitizer};
 use clap::Parser;
-use tracing::{info, Level};
+use tracing::{info, level_filters::LevelFilter, Level};
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
 use tracing_panic::panic_hook;
 use tracing_subscriber::{fmt, FmtSubscriber, layer::SubscriberExt};
@@ -25,65 +25,35 @@ run cargo install --version 0.14.0 iai-callgrind-runner
 fn iai_main() {
     env::set_var("RUST_BACKTRACE", "full");
 
-    let log_level = LogLevel::INFO;
-    let log_level = match log_level {
-        LogLevel::TRACE => Level::TRACE,
-        LogLevel::DEBUG => Level::DEBUG,
-        LogLevel::INFO => Level::INFO,
-        LogLevel::WARN => Level::WARN,
-        LogLevel::ERROR => Level::ERROR,
-    };
+    tracing_subscriber::fmt::init();
 
-    let mut exe_dir = env::current_exe().expect("Unable to get binary directory... aborting");
-    exe_dir.pop();
+    let COMMUNITY_PATH = env::var("COMMUNITY_PATH").unwrap_or("/home/odoo/Documents/odoo-servers/test-odoo/odoo".to_string());
+    let mut server = odoo_ls_server::core::odoo::SyncOdoo::new();
+    let (s, r) = crossbeam_channel::unbounded();
+    let mut session = odoo_ls_server::threads::SessionInfo::new_from_custom_channel(s.clone(), r.clone(), &mut server, None);
+    session.sync_odoo.load_odoo_addons = false;
 
-    let mut log_dir = exe_dir.join("logs").sanitize();
+    let addons_paths = vec![];
+    info!("Using addons path: {:?}", addons_paths);
 
-    let file_appender = RollingFileAppender::builder()
-        .max_log_files(5) // only the most recent 5 log files will be kept
-        .rotation(Rotation::HOURLY)
-        .filename_prefix("odoo_logs")
-        .filename_suffix(format!("{}.log", std::process::id()))
-        .build(log_dir)
-        .expect("failed to initialize rolling file appender");
-    let (file_writer, _guard) = tracing_appender::non_blocking(file_appender);
-    let subscriber = FmtSubscriber::builder()
-        .with_thread_ids(true)
-        .with_file(false)
-        .with_max_level(log_level)
-        .with_ansi(false)
-        .with_writer(file_writer)
-        .finish();
-    let stdout_subscriber = fmt::layer().with_writer(std::io::stdout).with_ansi(true);
-    tracing::subscriber::set_global_default(subscriber.with(stdout_subscriber)).expect("Unable to set default tracing subscriber");
-    ctrlc::set_handler(move || {
-        info!("Received ctrl-c signal");
-        std::process::exit(0);
-    })
-    .expect("Error setting Ctrl-C handler");
+    let workspace_folders = vec![COMMUNITY_PATH.clone()];
+    info!("Using tracked folders: {:?}", workspace_folders);
 
-    info!(">>>>>>>>>>>>>>>>>> New Session <<<<<<<<<<<<<<<<<<");
-    info!("Server version: {}", EXTENSION_VERSION);
-    info!("Compiled setting: DEBUG_ODOO_BUILDER: {}", DEBUG_ODOO_BUILDER);
-    info!("Compiled setting: DEBUG_MEMORY: {}", DEBUG_MEMORY);
-    info!("Operating system: {}", std::env::consts::OS);
-    info!("");
-    info!("starting server");
-    info!(tag = "test", "starting server (debug mode)");
-    let mut serv = Server::new_tcp().expect("Unable to start tcp connection");
-    serv.initialize().expect("Error while initializing server");
-    let sender_panic = serv.connection.as_ref().unwrap().sender.clone();
-    std::panic::set_hook(Box::new(move |panic_info| {
-        panic_hook(panic_info);
-        let _ = sender_panic.send(lsp_server::Message::Notification(Notification{
-            method: "Odoo/displayCrashNotification".to_string(),
-            params: json!({
-                "crashInfo": format!("{panic_info}"),
-                "pid": std::process::id()
-            })
-        }));
-    }));
-    black_box(serv.run(None));
+    for tracked_folder in workspace_folders {
+        session.sync_odoo.get_file_mgr().borrow_mut().add_workspace_folder(PathBuf::from(tracked_folder).sanitize());
+    }
+
+    let mut config = odoo_ls_server::core::config::Config::new();
+    config.addons = addons_paths;
+    config.odoo_path = COMMUNITY_PATH;
+    config.python_path = odoo_ls_server::S!("python3");
+    config.refresh_mode = odoo_ls_server::core::config::RefreshMode::Off;
+    config.diag_missing_imports = odoo_ls_server::core::config::DiagMissingImportsMode::All;
+    config.no_typeshed = false;
+    config.additional_stubs = vec![];
+    config.stdlib = "".to_string();
+
+    black_box(odoo_ls_server::core::odoo::SyncOdoo::init(&mut session, config));
     info!(">>>>>>>>>>>>>>>>>> End Session <<<<<<<<<<<<<<<<<<");
 }
 
