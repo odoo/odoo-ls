e5f12806143225615eb6d88f53bae0efa8849eab
fda-odoo <fda@odoo.com>
2025-03-03 14:24:47 +0100

    wip2

--- tools/odoolsspy/views/list_entries.py
+++ tools/odoolsspy/views/list_entries.py
@@ -1,14 +1,111 @@
 import dearpygui.dearpygui as dpg
 import json
 
-def create_entry_window(connection):
-    with dpg.window(label="Entry list", width=300, height=300) as entry_window:
-        message = {
-            "jsonrpc": "2.0",
-            "method": "toolAPI/list_entries",
-            "params": {},
-            "id": 1
-        }
-        connection.send(json.dumps(message).encode('utf-8'))
+class EntryTab():
+
+    def __init__(self):
+        self.entries = []
+        self.tab_ids = 0
+        self.is_theme_setup = False
+
+    def setup_tab(self, app, connection_mgr):
+        id = connection_mgr.send_message("$/ToolAPI/list_entries", {})
+
+        response = connection_mgr.get_response(id)
+        if dpg.does_item_exist("entry_tab"):
+            dpg.delete_item("entry_tab")
         
-        dpg.add_text("Sent LSP initialize message")
+        self.setup_theme()
+
+        dpg.add_tab(tag="entry_tab", parent="left_tab_bar", label="EntryPoints", closable=True)
+        if "result" in response:
+            entries = response["result"]
+            for entry in entries:
+                self.entries.append(Entry(entry, self.tab_ids, "entry_tab"))
+                self.tab_ids += 1
+        else:
+            dpg.add_text("Failed to retrieve entry points", parent="entry_tab")
+
+    def setup_theme(self):
+        if self.is_theme_setup:
+            return
+        self.is_theme_setup = True
+
+        with dpg.theme(tag="header_theme_main"):
+            with dpg.theme_component(dpg.mvCollapsingHeader):
+                dpg.add_theme_color(dpg.mvThemeCol_Header, (41, 107, 31, 255), category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_HeaderHovered, (63, 161, 48, 255), category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_HeaderActive, (82, 209, 63, 255), category=dpg.mvThemeCat_Core)
+
+        with dpg.theme(tag="header_theme_addon"):
+            with dpg.theme_component(dpg.mvCollapsingHeader):
+                dpg.add_theme_color(dpg.mvThemeCol_Header, (32, 110, 63, 255), category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_HeaderHovered, (41, 143, 82, 255), category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_HeaderActive, (49, 168, 97, 255), category=dpg.mvThemeCat_Core)
+
+        with dpg.theme(tag="header_theme_public"):
+            with dpg.theme_component(dpg.mvCollapsingHeader):
+                dpg.add_theme_color(dpg.mvThemeCol_Header, (112, 85, 31, 255), category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_HeaderHovered, (128, 97, 36, 255), category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_HeaderActive, (143, 108, 40, 255), category=dpg.mvThemeCat_Core)
+
+        with dpg.theme(tag="header_theme_builtin"):
+            with dpg.theme_component(dpg.mvCollapsingHeader):
+                dpg.add_theme_color(dpg.mvThemeCol_Header, (32, 51, 115, 255), category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_HeaderHovered, (39, 61, 138, 255), category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_HeaderActive, (43, 67, 153, 255), category=dpg.mvThemeCat_Core)
+
+        with dpg.theme(tag="header_theme_custom"):
+            with dpg.theme_component(dpg.mvCollapsingHeader):
+                dpg.add_theme_color(dpg.mvThemeCol_Header, (33, 99, 117, 255), category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_HeaderHovered, (39, 119, 140, 255), category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_HeaderActive, (47, 143, 168, 255), category=dpg.mvThemeCat_Core)
+
+
+class Entry():
+
+    def __init__(self, entry, id, parent):
+        self.entry = entry
+        entry_path_split = entry["path"].split("/")
+        entry_name = entry["type"] + ": " + (".../" if len(entry_path_split) > 4 else "") + "/".join(entry_path_split[-4:])
+        dpg.add_collapsing_header(tag="col_header_" + str(id), label=entry_name, parent=parent)
+        dpg.bind_item_font("col_header_" + str(id), "arial14")
+        dpg.bind_item_theme("col_header_" + str(id), "header_theme_" + entry["type"])
+        with dpg.group(parent="col_header_" + str(id), horizontal_spacing=10, horizontal=True):
+            dpg.add_spacer(width=10)
+            with dpg.group():
+                with dpg.group(horizontal=True):
+                    text = dpg.add_text("Path: ")
+                    dpg.bind_item_font(text, "arialbd14")
+                    text = dpg.add_text(entry["path"])
+                    dpg.bind_item_font(text, "arial14")
+                with dpg.group(horizontal=True):
+                    text = dpg.add_text("Tree: ")
+                    dpg.bind_item_font(text, "arialbd14")
+                    text = dpg.add_text(entry["tree"])
+                    dpg.bind_item_font(text, "arial14")
+                if entry["addon_to_odoo_path"]:
+                    with dpg.group(horizontal=True):
+                        text = dpg.add_text("Addon to Odoo Path: ")
+                        dpg.bind_item_font(text, "arialbd14")
+                        text = dpg.add_text(entry["addon_to_odoo_path"])
+                        dpg.bind_item_font(text, "arial14")
+                if entry["addon_to_odoo_tree"]:
+                    with dpg.group(horizontal=True):
+                        text = dpg.add_text("Addon to Odoo Tree: ")
+                        dpg.bind_item_font(text, "arialbd14")
+                        text = dpg.add_text(entry["addon_to_odoo_tree"])
+                        dpg.bind_item_font(text, "arial14")
+                if len(entry["not_found_symbols"]) > 0:
+                    text_symbol = dpg.add_text("Not found symbols: ")
+                    dpg.bind_item_font(text_symbol, "arialbd14")
+                    for symbol in entry["not_found_symbols"]:
+                        with dpg.group(horizontal=True):
+                            dpg.add_spacer(width=10)
+                            text = dpg.add_text(symbol)
+                            dpg.bind_item_font(text, "arial14")
+                            dpg.add_button(label="Go to", callback=lambda: self.go_to_symbol(symbol))
+
+    def go_to_symbol(self, symbol):
+        from views.symbols import Symbol
+        symbol = Symbol(symbol[1][-1] if symbol[1] else symbol[0][-1], symbol)
--- tools/odoolsspy/views/monitoring.py
+++ tools/odoolsspy/views/monitoring.py
@@ -2,49 +2,82 @@ import psutil
 import dearpygui.dearpygui as dpg
 import threading
 import time
+from connection import connection
 
-TARGET_NAME = "odoo_ls_server"
-running = True
+class Monitoring:
+    def __init__(self):
+        self.running = True
+        self.window_height_per_cpu = 75
+        self.theme_color_id = None
+        self.thread = None
 
-def get_cpu_usage_by_name(name):
-    processes = {}
-    for process in psutil.process_iter(attrs=['pid', 'name']):
-        try:
-            if name.lower() in process.info['name'].lower():
-                proc = psutil.Process(process.info['pid'])
-                cpu_usage = proc.cpu_percent(interval=1.0)  # Attendre pour avoir une vraie mesure
-                processes[proc.pid] = cpu_usage
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+    def get_cpu_usage_by_name(self, name):
+        processes = {}
+        for process in psutil.process_iter(attrs=['pid', 'name']):
+            try:
+                if name.lower() in process.info['name'].lower():
+                    proc = psutil.Process(process.info['pid'])
+                    cpu_usage = proc.cpu_percent(interval=1.0)
+                    processes[proc.pid] = cpu_usage
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        return processes
+
+    def update_cpu_usage(self):
+        while self.running:
+            cpu_usages = self.get_cpu_usage_by_name("odoo_ls_server")
+
+            for pid, cpu in cpu_usages.items():
+                if not dpg.does_item_exist(f"progress_{pid}"):
+                    with dpg.group(parent="cpu_window"):
+                        dpg.add_progress_bar(default_value=0.0, tag=f"progress_{pid}", width=200, height=10)
+                        text = dpg.add_text(f"PID {pid}:", tag=f"label_{pid}", pos=(10, 2))
+                        dpg.bind_item_font(text, "arial11")
+                    dpg.set_item_height("cpu_window", self.window_height_per_cpu * len(cpu_usages))
+
+                dpg.set_value(f"progress_{pid}", cpu / 100.0)
+                dpg.configure_item(f"label_{pid}", default_value=f"PID {pid}: {cpu:.2f}% CPU")
+
+            existing_pids = {int(tag.split("_")[1]) for tag in dpg.get_all_items() if dpg.get_item_label(tag).startswith("progress_")}
+            for pid in existing_pids - cpu_usages.keys():
+                dpg.delete_item(f"label_{pid}")
+                dpg.delete_item(f"progress_{pid}")
+
+            time.sleep(1)
+
+    def close_window(self):
+        self.running = False
+        if self.thread:
+            self.thread.join()
+        dpg.delete_item("cpu_window")
+
+    def start(self):
+        with dpg.window(label="CPU Usage", tag="cpu_window", width=200, height=self.window_height_per_cpu, no_title_bar=True, on_close=self.close_window, no_move=True, no_resize=True):
             pass
-    return processes
+        with dpg.theme() as bg_window_theme:
+            with dpg.theme_component(dpg.mvWindowAppItem):
+                dpg.add_theme_style(dpg.mvStyleVar_WindowPadding, 5, 5, parent=dpg.last_item())
+                self.theme_color_id = dpg.add_theme_color(dpg.mvThemeCol_WindowBg, (30, 30, 30, 255), category=dpg.mvThemeCat_Core)
 
-def update_cpu_usage():
-    global running
-    while running:
-        cpu_usages = get_cpu_usage_by_name(TARGET_NAME)
+        dpg.bind_item_theme("cpu_window", bg_window_theme)
+        self.thread = threading.Thread(target=self.update_cpu_usage, daemon=True)
+        self.thread.start()
+        self.update_window_position()
 
-        for pid, cpu in cpu_usages.items():
-            if not dpg.does_item_exist(f"progress_{pid}"):
-                with dpg.group(parent="cpu_window"):
-                    dpg.add_text(f"PID {pid}:", tag=f"label_{pid}")
-                    dpg.add_progress_bar(default_value=0.0, tag=f"progress_{pid}", width=300)
+    def update_window_position(self):
+        viewport_width = dpg.get_viewport_width()
+        viewport_height = dpg.get_viewport_height()
+        window_height = 20
 
-            dpg.set_value(f"progress_{pid}", cpu / 100.0)  # Valeur entre 0.0 et 1.0
-            dpg.configure_item(f"label_{pid}", default_value=f"PID {pid}: {cpu:.2f}% CPU")
+        y_pos = viewport_height - window_height - 20
 
-        existing_pids = {int(tag.split("_")[1]) for tag in dpg.get_all_items() if dpg.get_item_label(tag).startswith("progress_")}
-        for pid in existing_pids - cpu_usages.keys():
-            dpg.delete_item(f"label_{pid}")
-            dpg.delete_item(f"progress_{pid}")
+        dpg.set_item_width("cpu_window", viewport_width)
+        dpg.set_item_pos("cpu_window", (0, y_pos))
 
-        time.sleep(1)
+    def toggle_cpu_usage(self):
+        dpg.set_value("cpu_wdw_visibility", not dpg.get_value("cpu_wdw_visibility"))
+        cpu_window_visibility = dpg.get_value("cpu_wdw_visibility")
+        dpg.configure_item("cpu_window", show=cpu_window_visibility)
 
-def close_window():
-    global running
-    running = False
-    dpg.delete_item("cpu_window")
-
-def monitoring():
-    with dpg.window(label=f"CPU Usage", tag="cpu_window", no_title_bar=True, on_close=close_window):
-        pass
-    threading.Thread(target=update_cpu_usage, daemon=True).start()
+    def set_busy(self, is_busy):
+        dpg.set_value(self.theme_color_id, (210, 170, 0, 255) if is_busy else (30, 230, 30, 255))
