8365dc1b3577d4d852537fac0cb7edc485592151
fda-odoo <fda@odoo.com>
2025-03-11 14:39:59 +0100

    [IMP] server: improve tests

--- server/tests/test_ls.rs
+++ server/tests/test_ls.rs
@@ -1,6 +1,6 @@
 
 
-use std::rc::Rc;
+use std::{env, rc::Rc};
 use std::cell::RefCell;
 use std::fs::File;
 use std::io::BufReader;
@@ -16,29 +16,31 @@ fn test_structure() {
     /* First, let's launch the server. It will setup a SyncOdoo struct, with a SyncChannel, that we can use to get the messages that the client would receive. */
     let odoo = setup::setup::setup_server(true);
 
-    // assert!(odoo.get_symbol(&(vec![S!("odoo")], vec![])).is_some());
-    // assert!(odoo.get_symbol(&(vec![S!("odoo"), S!("addons")], vec![])).is_some());
-    // assert!(odoo.get_symbol(&(vec![S!("odoo"), S!("addons"), S!("module_1")], vec![])).is_some());
-    // assert!(odoo.get_symbol(&(vec![S!("odoo"), S!("addons"), S!("module_2")], vec![])).is_some());
-    // assert!(odoo.get_symbol(&(vec![S!("odoo"), S!("addons"), S!("not_a_module")], vec![])).is_none());
+    let odoo_path = env::var("COMMUNITY_PATH").unwrap();
+    let odoo_path = odoo_path.as_str();
+    assert!(!odoo.get_symbol(odoo_path, &(vec![S!("odoo")], vec![]), u32::MAX).is_empty());
+    assert!(!odoo.get_symbol(odoo_path, &(vec![S!("odoo"), S!("addons")], vec![]), u32::MAX).is_empty());
+    assert!(!odoo.get_symbol(odoo_path, &(vec![S!("odoo"), S!("addons"), S!("module_1")], vec![]), u32::MAX).is_empty());
+    assert!(!odoo.get_symbol(odoo_path, &(vec![S!("odoo"), S!("addons"), S!("module_2")], vec![]), u32::MAX).is_empty());
+    assert!(odoo.get_symbol(odoo_path, &(vec![S!("odoo"), S!("addons"), S!("not_a_module")], vec![]), u32::MAX).is_empty());
 
-    // assert!(odoo.get_symbol(&(vec![S!("odoo"), S!("addons"), S!("module_1"), S!("not_loaded")], vec![])).is_none());
-    // assert!(odoo.get_symbol(&(vec![S!("odoo"), S!("addons"), S!("module_1"), S!("not_loaded"), S!("not_loaded_file")], vec![])).is_none());
-    // assert!(odoo.get_symbol(&(vec![S!("odoo"), S!("addons"), S!("module_1"), S!("not_loaded"), S!("not_loaded_file")], vec![S!("NotLoadedClass")])).is_none());
-    // assert!(odoo.get_symbol(&(vec![S!("odoo"), S!("addons"), S!("module_1"), S!("not_loaded"), S!("not_loaded_file")], vec![S!("NotLoadedFunc")])).is_none());
+    assert!(odoo.get_symbol(odoo_path, &(vec![S!("odoo"), S!("addons"), S!("module_1"), S!("not_loaded")], vec![]), u32::MAX).is_empty());
+    assert!(odoo.get_symbol(odoo_path, &(vec![S!("odoo"), S!("addons"), S!("module_1"), S!("not_loaded"), S!("not_loaded_file")], vec![]), u32::MAX).is_empty());
+    assert!(odoo.get_symbol(odoo_path, &(vec![S!("odoo"), S!("addons"), S!("module_1"), S!("not_loaded"), S!("not_loaded_file")], vec![S!("NotLoadedClass")]), u32::MAX).is_empty());
+    assert!(odoo.get_symbol(odoo_path, &(vec![S!("odoo"), S!("addons"), S!("module_1"), S!("not_loaded"), S!("not_loaded_file")], vec![S!("NotLoadedFunc")]), u32::MAX).is_empty());
 
-    // assert!(odoo.get_symbol(&(vec![S!("odoo"), S!("addons"), S!("module_1"), S!("models")], vec![])).is_some());
-    // let models = odoo.get_symbol(&(vec![S!("odoo"), S!("addons"), S!("module_1"), S!("models")], vec![])).unwrap();
-    // assert!(models.borrow().get_symbol(&(vec![S!("base_test_models")], vec![])).is_some());
-    // assert!(models.borrow().get_symbol(&(vec![], vec![S!("base_test_models")])).is_some());
-    // assert!(!Rc::ptr_eq(&models.borrow().get_symbol(&(vec![S!("base_test_models")], vec![])).unwrap(),
-    //         &models.borrow().get_symbol(&(vec![], vec![S!("base_test_models")])).unwrap()));
-    // assert!(Rc::ptr_eq(&models.borrow().symbols["base_test_models"], &models.borrow().get_symbol(&(vec![], vec![S!("base_test_models")])).unwrap()));
-    // assert!(Rc::ptr_eq(&models.borrow().module_symbols["base_test_models"], &models.borrow().get_symbol(&(vec![S!("base_test_models")], vec![])).unwrap()));
-    // let module_1 = odoo.get_symbol(&(vec![S!("odoo"), S!("addons"), S!("module_1")], vec![])).unwrap();
-    // assert!(compare_symbol_with_json(module_1, "tests/module_1_structure.json"))
+    let models = odoo.get_symbol(odoo_path, &(vec![S!("odoo"), S!("addons"), S!("module_1"), S!("models")], vec![]), u32::MAX);
+    assert!(!models.len() == 1);
+    assert!(models[0].borrow().get_symbol(&(vec![S!("base_test_models")], vec![]), u32::MAX).len() == 1);
+    assert!(models[0].borrow().get_symbol(&(vec![], vec![S!("base_test_models")]), u32::MAX).len() == 1);
+    assert!(!Rc::ptr_eq(&models[0].borrow().get_symbol(&(vec![S!("base_test_models")], vec![]), u32::MAX)[0],
+            &models[0].borrow().get_symbol(&(vec![], vec![S!("base_test_models")]), u32::MAX)[0]));
+    let module_1 = odoo.get_symbol(odoo_path, &(vec![S!("odoo"), S!("addons"), S!("module_1")], vec![]), u32::MAX);
+    assert!(module_1.len() == 1);
+    //assert!(compare_symbol_with_json(module_1, "tests/module_1_structure.json"))
 }
 
+/*
 fn compare_symbol_with_json(symbol: Rc<RefCell<Symbol>>, json_path: &str) -> bool {
     let file = File::open(json_path).expect("File not found");
     let reader = BufReader::new(file);
@@ -182,3 +184,4 @@ fn _test_symbol_with_json_value(symbol: Rc<RefCell<Symbol>>, json: Value) -> boo
     //return result
     is_ok
 }
+*/
