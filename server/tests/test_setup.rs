b8ad24e22d02d1ddaa63ad477cda598f1d4d467f
M Mahrous <mdms@odoo.com>
2025-03-25 17:49:47 +0100

    [FIX] server: fix community path in tests

--- server/tests/test_setup.rs
+++ server/tests/test_setup.rs
@@ -1,8 +1,9 @@
 use std::env;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use odoo_ls_server::constants::OYarn;
-use odoo_ls_server::{Sy, S};
+use odoo_ls_server::utils::PathSanitizer;
+use odoo_ls_server::Sy;
 
 mod setup;
 
@@ -21,10 +22,12 @@ fn test_start_odoo_server() {
     /* First, let's launch the server. It will setup a SyncOdoo struct, with a SyncChannel, that we can use to get the messages that the client would receive. */
     let odoo = setup::setup::setup_server(true);
 
+    let odoo_path = PathBuf::from(env::var("COMMUNITY_PATH").unwrap()).sanitize();
+
     /* Let's ensure that the architecture is loaded */
-    assert!(!odoo.get_symbol(env::var("COMMUNITY_PATH").unwrap().as_str(), &(vec![Sy!("odoo")], vec![]), u32::MAX).is_empty());
+    assert!(!odoo.get_symbol(odoo_path.as_str(), &(vec![Sy!("odoo")], vec![]), u32::MAX).is_empty());
     /* Let's ensure that odoo/addons is loaded */
-    assert!(!odoo.get_symbol(env::var("COMMUNITY_PATH").unwrap().as_str(), &(vec![Sy!("odoo"), Sy!("addons")], vec![]), u32::MAX).is_empty());
+    assert!(!odoo.get_symbol(odoo_path.as_str(), &(vec![Sy!("odoo"), Sy!("addons")], vec![]), u32::MAX).is_empty());
     /* And let's test that our test module has well been added and available in odoo/addons */
-    assert!(!odoo.get_symbol(env::var("COMMUNITY_PATH").unwrap().as_str(), &(vec![Sy!("odoo"), Sy!("addons"), Sy!("module_1")], vec![]), u32::MAX).is_empty());
+    assert!(!odoo.get_symbol(odoo_path.as_str(), &(vec![Sy!("odoo"), Sy!("addons"), Sy!("module_1")], vec![]), u32::MAX).is_empty());
 }
