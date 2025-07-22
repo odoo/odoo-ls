5c984ff1de023f539024d20d5a8fcda2e6e11c4a
fda-odoo <fda@odoo.com>
2025-07-22 17:55:01 +0200

    [IMP] server: basic field diagnostic structure

--- server/src/features/xml_ast_utils.rs
+++ server/src/features/xml_ast_utils.rs
@@ -54,6 +54,8 @@ impl XmlAstUtils {
                     }
                 }
             }
+        } else if node.is_text() {
+            XmlAstUtils::visit_text(session, &node, offset, from_module, ctxt, results);
         }
     }
 
@@ -61,7 +63,7 @@ impl XmlAstUtils {
         for attr in node.attributes() {
             if attr.name() == "model" {
                 let model_name = attr.value().to_string();
-                ctxt.insert(S!("model"), ContextValue::STRING(model_name.clone()));
+                ctxt.insert(S!("record_model"), ContextValue::STRING(model_name.clone()));
                 if attr.range_value().start <= offset && attr.range_value().end >= offset {
                     if let Some(model) = session.sync_odoo.models.get(&model_name).cloned() {
                         results.0.extend(model.borrow().all_symbols(session, from_module.clone(), false).iter().filter(|s| s.1.is_none()).map(|s| XmlAstResult::SYMBOL(s.0.clone())));
@@ -70,29 +72,23 @@ impl XmlAstUtils {
                 }
             } else if attr.name() == "id" {
                 if attr.range_value().start <= offset && attr.range_value().end >= offset {
-                    let xml_ids = session.sync_odoo.get_xml_ids(&from_module.clone().unwrap(), attr.value(), &attr.range(), &mut vec![]);
-                    for xml_data in xml_ids.iter() {
-                        match xml_data {
-                            XmlData::RECORD(r) => {
-                                results.0.push(XmlAstResult::XML_DATA(r.file_symbol.upgrade().unwrap(), r.range.clone()));
-                                results.1 = Some(attr.range_value());
-                            },
-                            _ => {}
-                        }
-                    }
+                    XmlAstUtils::add_xml_id_result(session, attr.value(), &from_module.as_ref().unwrap(), attr.range_value(), results);
+                    results.1 = Some(attr.range_value());
                 }
             }
         }
         for child in node.children() {
             XmlAstUtils::visit_node(session, &child, offset, from_module.clone(), ctxt, results);
         }
+        ctxt.remove(&S!("record_model"));
     }
 
     fn visit_field(session: &mut SessionInfo<'_>, node: &Node, offset: usize, from_module: Option<Rc<RefCell<Symbol>>>, ctxt: &mut HashMap<String, ContextValue>, results: &mut (Vec<XmlAstResult>, Option<Range<usize>>)) {
         for attr in node.attributes() {
-            if attr.range_value().start <= offset && attr.range_value().end >= offset {
-                if attr.name() == "name" {
-                    let model_name = ctxt.get(&S!("model")).cloned().unwrap_or(ContextValue::STRING(S!(""))).as_string();
+            if attr.name() == "name" {
+                ctxt.insert(S!("field_name"), ContextValue::STRING(attr.value().to_string()));
+                if attr.range_value().start <= offset && attr.range_value().end >= offset {
+                    let model_name = ctxt.get(&S!("record_model")).cloned().unwrap_or(ContextValue::STRING(S!(""))).as_string();
                     if model_name.is_empty() {
                         continue;
                     }
@@ -108,10 +104,54 @@ impl XmlAstUtils {
                         results.1 = Some(attr.range_value());
                     }
                 }
+            } else if attr.name() == "ref" {
+                if attr.range_value().start <= offset && attr.range_value().end >= offset {
+                    let mut field_name = "";
+                    for attr in node.attributes() {
+                        if attr.name() == "name" {
+                            field_name = attr.value();
+                        }
+                    }
+                    if field_name == "inherit_id" {
+                        XmlAstUtils::add_xml_id_result(session, attr.value(), &from_module.as_ref().unwrap(), attr.range_value(), results);
+                        results.1 = Some(attr.range_value());
+                    }
+                }
             }
         }
         for child in node.children() {
             XmlAstUtils::visit_node(session, &child, offset, from_module.clone(), ctxt, results);
+        }
+        ctxt.remove(&S!("field_name"));
+    }
+
+    fn visit_text(session: &mut SessionInfo, node: &Node, offset: usize, from_module: Option<Rc<RefCell<Symbol>>>, ctxt: &mut HashMap<String, ContextValue>, results: &mut (Vec<XmlAstResult>, Option<Range<usize>>)) {
+        if node.range().start <= offset && node.range().end >= offset {
+            let model = ctxt.get(&S!("record_model")).cloned().unwrap_or(ContextValue::STRING(S!(""))).as_string();
+            let field = ctxt.get(&S!("field_name")).cloned().unwrap_or(ContextValue::STRING(S!(""))).as_string();
+            if model.is_empty() || field.is_empty() {
+                return;
+            }
+            if model == "ir.ui.view" {
+                if field == "model" {
+                    if let Some(model) = session.sync_odoo.models.get(node.text().unwrap()).cloned() {
+                        results.0.extend(model.borrow().all_symbols(session, from_module.clone(), false).iter().filter(|s| s.1.is_none()).map(|s| XmlAstResult::SYMBOL(s.0.clone())));
+                        results.1 = Some(node.range());
+                    }
+                }
+            }
+        }
+    }
+
+    fn add_xml_id_result(session: &mut SessionInfo, xml_id: &str, file_symbol: &Rc<RefCell<Symbol>>, range: Range<usize>, results: &mut (Vec<XmlAstResult>, Option<Range<usize>>)) {
+        let xml_ids = session.sync_odoo.get_xml_ids(file_symbol, xml_id, &range, &mut vec![]);
+        for xml_data in xml_ids.iter() {
+            match xml_data {
+                XmlData::RECORD(r) => {
+                    results.0.push(XmlAstResult::XML_DATA(r.file_symbol.upgrade().unwrap(), r.range.clone()));
+                },
+                _ => {}
+            }
         }
     }
 
