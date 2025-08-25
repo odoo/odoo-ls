f2fa63a1bdaf89960e71d982073464b0c145c335
fda-odoo <fda@odoo.com>
2025-08-25 17:40:35 +0200

    wip

--- server/src/core/csv_arch_builder.rs
+++ server/src/core/csv_arch_builder.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, fs::File, rc::Rc};
 
 use lsp_types::{Diagnostic};
 
@@ -20,6 +20,14 @@ impl CsvArchBuilder {
         let mut diagnostics = vec![];
         csv_symbol.borrow_mut().set_build_status(BuildSteps::ARCH, BuildStatus::IN_PROGRESS);
         //TODO load csv file
+        let mut rdr = csv::Reader::from_reader(content.as_bytes());
+        for result in rdr.records() {
+            // The iterator yields Result<StringRecord, Error>, so we check the
+            // error here.
+            if let Ok(record) = result {
+                println!("{:?}", record);
+            }
+        }
         csv_symbol.borrow_mut().set_build_status(BuildSteps::ARCH, BuildStatus::DONE);
         diagnostics
     }
