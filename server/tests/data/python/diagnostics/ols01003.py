601b1ff6ed6d5b3e211f350f8a8e142e7545d0ae
fda-odoo <fda@odoo.com>
2025-11-07 15:56:09 +0100

    [IMP] Test for ols01003

---
+++ server/tests/data/python/diagnostics/ols01003.py
@@ -0,0 +1,12 @@
+if input():
+    class A:
+        pass
+else:
+    class A:
+        pass
+
+class Test(A):
+    pass
+
+class Test2(Test):
+    pass
--- server/tests/diagnostics/mod.rs
+++ server/tests/diagnostics/mod.rs
@@ -2,3 +2,4 @@
 pub mod ols01000;
 pub mod ols01001;
 pub mod ols01002;
+pub mod ols01003;
