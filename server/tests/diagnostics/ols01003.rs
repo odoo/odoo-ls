6affc3934606ae93e429fa78b5db4fe00eda27f2
fda-odoo <fda@odoo.com>
2025-11-07 15:56:09 +0100

    [IMP] Test for ols01003

---
+++ server/tests/diagnostics/ols01003.rs
@@ -0,0 +1,24 @@
+use std::env;
+
+use lsp_types::{DiagnosticSeverity, NumberOrString};
+use odoo_ls_server::{S, utils::PathSanitizer};
+
+use crate::setup::setup::*;
+
+#[test]
+fn test_ols01003() {
+    let mut odoo = setup_server(false);
+    let path = env::current_dir().unwrap().join("tests/data/python/diagnostics/ols01003.py").sanitize();
+    let mut session = prepare_custom_entry_point(&mut odoo, &path);
+    let diagnostics = get_diagnostics_for_path(&mut session, &path);
+    assert_eq!(diagnostics.len(), 1);
+    let diag = &diagnostics[0];
+    assert!(diag.code.is_some());
+    let code = match &diag.code {
+        Some(NumberOrString::String(code)) => code,
+        Some(NumberOrString::Number(num)) => panic!("Unexpected numeric code: {}", num),
+        None => panic!("Diagnostic code is None"),
+    };
+    assert!(code == &S!("OLS01003"));
+    assert!(diag.severity.is_some_and(|s| s == DiagnosticSeverity::WARNING));
+}
