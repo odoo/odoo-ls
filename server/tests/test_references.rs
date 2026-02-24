637a631b280b0c1f77fd1d175f71c05cf256cdf0
fda-odoo <fda@odoo.com>
2026-02-24 11:36:51 +0100

    [FIX] fix duplicate references and order in tests

--- server/tests/test_references.rs
+++ server/tests/test_references.rs
@@ -1,8 +1,7 @@
 use std::path::PathBuf;
 
-use lsp_types::{Location, PartialResultParams, Position, TextDocumentContentChangeEvent, WorkDoneProgressParams};
-use odoo_ls_server::{core::file_mgr::FileMgr, features::references, threads::SessionInfo, utils::PathSanitizer};
-use tracing::error;
+use lsp_types::{Location, PartialResultParams, Position, WorkDoneProgressParams};
+use odoo_ls_server::{core::file_mgr::FileMgr, threads::SessionInfo, utils::PathSanitizer};
 
 use crate::setup::setup::{create_init_session, setup_server};
 
@@ -18,51 +17,59 @@ fn test_references() {
 
     let test_file = test_addons_path.join("module_1").join("models").join("base_test_models.py").sanitize();
     //1. reference of a model
-    let references = get_references(&mut session, &test_file, Position::new(4, 29));
-    for r in references.iter() {
-        error!("Reference found at {}:{}:{}", r.uri.as_str(), r.range.start.line, r.range.start.character);
-    }
-    assert!(references.len() >= 10, "Expected at least 10 references, got {}", references.len());
-    for i in 0..9 {
-        assert!(references[i].uri.as_str().ends_with("base_test_models.py"), "Expected reference in base_test_models.py, got {}", references[i].uri.as_str());
-    }
-    assert!(references[0].range.start.line == 4 && references[0].range.start.character == 12, "Expected 1th reference at line 4, character 12, got line {}, character {}", references[0].range.start.line, references[0].range.start.character);
-    assert!(references[1].range.start.line == 15 && references[1].range.start.character == 8, "Expected 2nd reference at line 15, character 8, got line {}, character {}", references[1].range.start.line, references[1].range.start.character);
-    assert!(references[2].range.start.line == 32 && references[2].range.start.character == 17, "Expected 3th reference at line 32, character 17, got line {}, character {}", references[2].range.start.line, references[2].range.start.character);
-    assert!(references[3].range.start.line == 33 && references[3].range.start.character == 8, "Expected 4th reference at line 33, character 8, got line {}, character {}", references[3].range.start.line, references[3].range.start.character);
-    assert!(references[4].range.start.line == 34 && references[4].range.start.character == 18, "Expected 5th reference at line 34, character 18, got line {}, character {}", references[4].range.start.line, references[4].range.start.character);
-    assert!(references[5].range.start.line == 40 && references[5].range.start.character == 16, "Expected 6th reference at line 40, character 16, got line {}, character {}", references[5].range.start.line, references[5].range.start.character);
-    assert!(references[6].range.start.line == 41 && references[6].range.start.character == 16, "Expected 7th reference at line 41, character 16, got line {}, character {}", references[6].range.start.line, references[6].range.start.character);
-    assert!(references[7].range.start.line == 42 && references[7].range.start.character == 16, "Expected 8th reference at line 42, character 16, got line {}, character {}", references[7].range.start.line, references[7].range.start.character);
-    assert!(references[8].range.start.line == 43 && references[8].range.start.character == 16, "Expected 9th reference at line 43, character 16, got line {}, character {}", references[8].range.start.line, references[8].range.start.character);
-    assert!(references[9].uri.as_str().ends_with("module_2/models/base_test_models.py"), "Expected reference 10 to be in module2/base_test_models.py, got {}", references[9].uri.as_str());
-    assert!(references[10].range.start.line == 44 && references[10].range.start.character == 16, "Expected ninth reference at line 44, character 16, got line {}, character {}", references[10].range.start.line, references[10].range.start.character);
-    assert!(references.len() == 11, "Expected only 10 references, got unexpected additional references: {}",
-        references.iter().skip(11).map(|r| format!("{}:{}:{}", r.uri.as_str(), r.range.start.line, r.range.start.character)).collect::<Vec<String>>().join(", ")
+    let mut references = get_references(&mut session, &test_file, Position::new(4, 29));
+    //as order is not guaranteed (usage of set), check if the expected reference is somewhere in the result list.
+    assert_in_result(&mut references, "module_1/models/base_test_models.py", 4, 12);
+    assert_in_result(&mut references, "module_1/models/base_test_models.py", 15, 8);
+    assert_in_result(&mut references, "module_1/models/base_test_models.py", 32, 17);
+    assert_in_result(&mut references, "module_1/models/base_test_models.py", 33, 8);
+    assert_in_result(&mut references, "module_1/models/base_test_models.py", 34, 18);
+    assert_in_result(&mut references, "module_1/models/base_test_models.py", 40, 16);
+    assert_in_result(&mut references, "module_1/models/base_test_models.py", 41, 16);
+    assert_in_result(&mut references, "module_1/models/base_test_models.py", 42, 16);
+    assert_in_result(&mut references, "module_1/models/base_test_models.py", 43, 16);
+    assert_in_result(&mut references, "module_1/models/diagnostics.py", 9, 34);
+    assert_in_result(&mut references, "module_1/models/diagnostics.py", 17, 34);
+    assert_in_result(&mut references, "module_1/models/diagnostics.py", 18, 34);
+    assert_in_result(&mut references, "module_1/models/diagnostics.py", 19, 34);
+    assert_in_result(&mut references, "module_1/models/diagnostics.py", 20, 34);
+    assert_in_result(&mut references, "module_1/models/diagnostics.py", 22, 34);
+    assert_in_result(&mut references, "module_2/models/base_test_models.py", 5, 15);
+    assert!(references.len() == 0, "Some references were not expected: {}",
+        references.iter().map(|r| format!("{}:{}:{}", r.uri.as_str(), r.range.start.line, r.range.start.character)).collect::<Vec<String>>().join(", ")
     );
 
     // reference of an attribute
-    let references = get_references(&mut session, &test_file, Position::new(9, 8));
-    for r in references.iter() {
-        error!("Reference found at {}:{}:{}", r.uri.as_str(), r.range.start.line, r.range.start.character);
-    }
-    assert!(references.len() >= 8, "Expected at least 8 references, got {}", references.len());
-    for i in 0..8 {
-        assert!(references[i].uri.as_str().ends_with("base_test_models.py"), "Expected reference in base_test_models.py, got {}", references[i].uri.as_str());
-    }
-    assert!(references[0].range.start.line == 15 && references[0].range.start.character == 8, "Expected first reference at line 15, character 8, got line {}, character {}", references[0].range.start.line, references[0].range.start.character);
-    assert!(references[1].range.start.line == 32 && references[1].range.start.character == 8, "Expected second reference at line 32, character 8, got line {}, character {}", references[1].range.start.line, references[1].range.start.character);
-    assert!(references[2].range.start.line == 33 && references[2].range.start.character == 8, "Expected third reference at line 33, character 8, got line {}, character {}", references[2].range.start.line, references[2].range.start.character);
-    assert!(references[3].range.start.line == 34 && references[3].range.start.character == 18, "Expected fourth reference at line 34, character 18, got line {}, character {}", references[3].range.start.line, references[3].range.start.character);
-    assert!(references[4].range.start.line == 40 && references[4].range.start.character == 16, "Expected fifth reference at line 40, character 16, got line {}, character {}", references[4].range.start.line, references[4].range.start.character);
-    assert!(references[5].range.start.line == 41 && references[5].range.start.character == 16, "Expected sixth reference at line 41, character 16, got line {}, character {}", references[5].range.start.line, references[5].range.start.character);
-    assert!(references[6].range.start.line == 42 && references[6].range.start.character == 16, "Expected seventh reference at line 42, character 16, got line {}, character {}", references[6].range.start.line, references[6].range.start.character);
-    assert!(references[7].range.start.line == 43 && references[7].range.start.character == 16, "Expected eighth reference at line 43, character 16, got line {}, character {}", references[7].range.start.line, references[7].range.start.character);
-    assert!(references.len() == 8, "Expected only 8 references, got unexpected additional references: {}",
-        references.iter().skip(8).map(|r| format!("{}:{}:{}", r.uri.as_str(), r.range.start.line, r.range.start.character)).collect::<Vec<String>>().join(", ")
+    let mut references = get_references(&mut session, &test_file, Position::new(9, 8));
+    assert_in_result(&mut references, "module_1/models/base_test_models.py", 37, 18);
+    assert!(references.len() == 0, "Some references were not expected: {}",
+        references.iter().map(|r| format!("{}:{}:{}", r.uri.as_str(), r.range.start.line, r.range.start.character)).collect::<Vec<String>>().join(", ")
     );
 
     //reference of a simple variable
+    let mut references = get_references(&mut session, &test_file, Position::new(50, 4));
+    assert_in_result(&mut references, "module_1/models/models.py", 1, 30);
+    assert!(references.len() == 0, "Some references were not expected: {}",
+        references.iter().map(|r| format!("{}:{}:{}", r.uri.as_str(), r.range.start.line, r.range.start.character)).collect::<Vec<String>>().join(", ")
+    );
+    
+    // for r in references.iter() {
+    //     error!("Reference found at {}:{}:{}", r.uri.as_str(), r.range.start.line, r.range.start.character);
+    // }
+}
+
+fn assert_in_result(references: &mut Vec<Location>, end_path: &str, line: u32, character: u32) {
+    let mut index = None;
+    for (i, r) in references.iter().enumerate() {
+        if r.uri.as_str().ends_with(end_path) && r.range.start.line == line && r.range.start.character == character {
+            index = Some(i);
+        }
+    }
+    if let Some(i) = index {
+        references.remove(i);
+    } else {
+        assert!(false, "Expected reference not found: {}:{}:{}", end_path, line, character);
+    }
 }
 
 fn get_references(session: &mut SessionInfo, path: &String, position: Position)-> Vec<Location> {
