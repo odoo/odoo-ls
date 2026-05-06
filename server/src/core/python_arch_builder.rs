c7336caa31875630dead353456eb7c7a1f938d1e
M Mahrous <mdms@odoo.com>
2026-05-06 13:28:57 +0200

    [FIX] Fix panic on itering symbols on Disk Dir
    This happens when you try to import from a directory in a custom entry point. that should be a namespace/package but is not resolved
    
    A task should follow to fix import issues in custom EntryPoints

--- server/src/core/python_arch_builder.rs
+++ server/src/core/python_arch_builder.rs
@@ -222,20 +222,19 @@ impl PythonArchBuilder {
                     }
                     let mut dep_to_add = vec![];
                     let sym_type = import_symbol.borrow().typ();
-                    if sym_type != SymType::COMPILED {
-                        if !Rc::ptr_eq(self.sym_stack.last().unwrap(), &import_symbol) { /*We have to check that the imported symbol is not the current one. It can
-                            happen for example in a .pyi that is importing the .pyd file with the same name. As both exists, odools will try to import the pyi a second time in the same file,
-                            and so create a borrow error here
-                            */
-                            let symbol = import_symbol.borrow();
-                            for (name, loc_syms) in symbol.iter_symbols() {
-                                if all_name_allowed || name_filter.contains(&name) {
-                                    let variable = self.sym_stack.last().unwrap().borrow_mut().add_new_variable(session, OYarn::from(name.clone()), &import_result.range);
-                                    let mut loc = variable.borrow_mut();
-                                    loc.as_variable_mut().is_import_variable = true;
-                                    loc.as_variable_mut().evaluations = Evaluation::from_sections(&symbol, loc_syms);
-                                    dep_to_add.push(variable.clone());
-                                }
+                    if ![SymType::COMPILED, SymType::DISK_DIR].contains(&sym_type) // DISK_DIR does not have iter_symbols and is a symptom of unresolved dirs in custom EPs
+                    && !Rc::ptr_eq(self.sym_stack.last().unwrap(), &import_symbol) { /*We have to check that the imported symbol is not the current one. It can
+                        happen for example in a .pyi that is importing the .pyd file with the same name. As both exists, odools will try to import the pyi a second time in the same file,
+                        and so create a borrow error here
+                        */
+                        let symbol = import_symbol.borrow();
+                        for (name, loc_syms) in symbol.iter_symbols() {
+                            if all_name_allowed || name_filter.contains(&name) {
+                                let variable = self.sym_stack.last().unwrap().borrow_mut().add_new_variable(session, OYarn::from(name.clone()), &import_result.range);
+                                let mut loc = variable.borrow_mut();
+                                loc.as_variable_mut().is_import_variable = true;
+                                loc.as_variable_mut().evaluations = Evaluation::from_sections(&symbol, loc_syms);
+                                dep_to_add.push(variable.clone());
                             }
                         }
                     }
