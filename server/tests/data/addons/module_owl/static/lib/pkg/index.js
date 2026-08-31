81bcee79ca3676b05dcc28da838852a363ae3c04
Rodolpho Lima <rcdl@odoo.com>
2026-08-31 21:34:08 +0200

    [TEST] integration tests for import resolution

---
+++ server/tests/data/addons/module_owl/static/lib/pkg/index.js
@@ -0,0 +1,6 @@
+/** @odoo-module */
+// A directory's `index.js` is importable as the directory: `@module_owl/../lib/pkg`.
+
+export function pkg_thing() {
+    return "pkg";
+}
---
+++ server/tests/data/addons/module_owl/static/src/imports/lib_imports.js
@@ -0,0 +1,8 @@
+/** @odoo-module **/
+import { aliased_thing } from "@fixture/aliased";
+// `@fixture/opted_out` is the alias of an `@odoo-module ignore` file, so it names nothing.
+import { opted_out_thing } from "@fixture/opted_out";
+
+export function all_lib_things() {
+    return [aliased_thing(), opted_out_thing()].join(" ");
+}
