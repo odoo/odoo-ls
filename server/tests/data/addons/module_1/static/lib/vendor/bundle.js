91cbe025a61dc2fc21b420b91fb175bbb21a9286
Rodolpho Lima <rcdl@odoo.com>
2026-09-02 20:48:20 +0200

    [TESTS] js_module_scope

---
+++ server/tests/data/addons/module_1/static/lib/vendor/bundle.js
@@ -0,0 +1,4 @@
+// A lib with no module header.
+// Having an export is not enough to be included in the program roots.
+
+export const vendorBundle = { version: 1 };
---
+++ server/tests/data/addons/module_2/static/src/scoped/local.js
@@ -0,0 +1,1 @@
+export const localHelper = () => 2;
